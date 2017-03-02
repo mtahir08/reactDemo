2fe7c0d173afbdc70b8d55386add9c57795df124
mtahir08 <mtahir.ps@gmail.com>
2017-03-03 00:06:40 +0500

    basic redux counter example

---
+++ basic-redux/basic-counter/src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>this is counter</div>
+        );
+    }
+}
+
+export default Counter;
---
+++ basic-redux/basic-counter/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Counter from './components/counter';
+
+import './index.css';
+function counter(state = 0, action) {
+
+  switch (action.type) {
+    case 'INCREMENT': {
+      return state += 1
+    }
+    case 'DECREMENT': {
+     return state -= 1
+    }
+    default: return state;
+  }
+}
+
+let store = createStore(counter);
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+
+store.dispatch({ type: 'INCREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+
+store.dispatch({ type: 'DECREMENT' })
+store.dispatch({ type: 'INCREMENT' })
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
