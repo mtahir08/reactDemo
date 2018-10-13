2b9e0256bd2026bbc4461c2916c5cd487e073502
Muhammad Tahir <mtahir.ps@gmail.com>
2018-10-14 02:18:32 +0500

    firebase, material UI added

--- fireapp/src/App.js
+++ fireapp/src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as firebase from 'firebase';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
+import './config/fbconfig';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.ref = firebase.database().ref();
+  }
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.container}>
+        <Card className={classes.card}>
+          <CardContent>
+            <TextField id="name" label="Name" fullWidth />
+            <br />
+            <TextField id="email" label="Email" fullWidth />
+            <br />
+            <TextField id="contact" label="Contact" fullWidth />
+          </CardContent>
+          <CardActions>
+            <Button variant="extendedFab" fullWidth color="primary">
+              Add User
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+const styles = {
+  card: {
+    minWidth: 300,
+    maxWidth: 500
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+};
+export default withStyles(styles)(App);
