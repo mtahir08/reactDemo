dba813be9462b83e0937f74f34407d8d0b54d1d1
Muhammad Tahir <mtahir.ps@gmail.com>
2019-09-29 11:27:09 +0500

    counter cli code upgrade

--- counter-cli/src/App.js
+++ counter-cli/src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 // Custom components
 import Counter from './counter';
 import TextInput from './input';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Counter />
-        <TextInput />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Counter />
+      <TextInput />
+    </div>
+  );
 }
 
 export default App;
