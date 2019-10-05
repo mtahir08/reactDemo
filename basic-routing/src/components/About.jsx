62b84a5172370103604dc846bece9ec975e2d816
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-05 16:16:38 +0500

    add history push in about file

--- basic-routing/src/components/About.jsx
+++ basic-routing/src/components/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const About = () => {
+const About = (props) => {
   return (
     <div>
       This is About page
+      <br />
+      <button onClick={() => {
+        setTimeout(() => {
+          props.history.push('/')
+        }, 2000)
+      }}>Goto Home</button>
+      <button onClick={() => { props.history.push('/contact') }}>Goto Contact</button>
     </div>
   );
 }
