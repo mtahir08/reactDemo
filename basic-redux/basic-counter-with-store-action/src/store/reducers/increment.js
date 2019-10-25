cfda3ad8970f906f6b6ebe7f5218c11c24713cfe
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-25 23:52:00 +0500

    reducer update

--- basic-redux/basic-counter-with-store-action/src/store/reducers/increment.js
+++ basic-redux/basic-counter-with-store-action/src/store/reducers/increment.js
@@ -5,13 +5,15 @@ const INITIAL_STATE = {
 }
 
 function incrementCounter(state = INITIAL_STATE, action) {
-    console.log("action.type", action.type);
     switch (action.type) {
-        
-        case ActionTypes.INCREMENT : {
-            let newState = Object.assign({}, state, { incrementStateValue: state.incrementStateValue + action.val });
-            return newState;
+
+        case ActionTypes.INCREMENT: {
+            return {
+                ...state,
+                incrementStateValue: state.incrementStateValue + action.val
+            }
         }
+
         default: return state;
     }
 }
