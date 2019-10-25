aab3aa95142094d901aaa479a53fda57ea554792
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-26 01:51:45 +0500

    create auth reducer

---
+++ class-app/src/Store/Reducers/Auth.js
@@ -0,0 +1,60 @@
+import { ActionTypes } from '../Actions';
+
+const INITIAL_STATE = {
+    isLoading: false,
+    user: {}
+
+}
+
+function AuthReducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+
+        /* ---------------------------------- LOGIN START--------------------------------- */
+        case ActionTypes.LOGIN: {
+            return {
+                ...state,
+                isLoading: true,
+                user: {},
+            }
+        }
+        case ActionTypes.LOGIN_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+                user: action.payload,
+            }
+        }
+        case ActionTypes.LOGIN_FAILED: {
+            return {
+                ...state,
+                isLoading: false,
+                user: {},
+            }
+        }
+
+        /* ------------------------------ SIGNUP START ------------------------------ */
+
+        case ActionTypes.SIGNUP: {
+            return {
+                ...state,
+                isLoading: true,
+                user: {},
+            }
+        }
+        case ActionTypes.SIGNUP_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+            }
+        }
+        case ActionTypes.SIGNUP_FAILED: {
+            return {
+                ...state,
+                isLoading: false,
+            }
+        }
+        default: return state;
+    }
+}
+
+export { AuthReducer };
