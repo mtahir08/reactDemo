8e859a73c0d9ce2fef8ce6f6634a0aab46aee01a
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-27 00:08:24 +0500

    removed console

--- class-app/src/common/CustomButton/CustomButton.jsx
+++ class-app/src/common/CustomButton/CustomButton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function CustomButton(props) {
-    console.log(props);
     return (
         <button className="btn" onClick={() => { props.clickHandler() }} >{props.name}</button>
     );
--- class-app/src/common/CustomInput/CustomInput.jsx
+++ class-app/src/common/CustomInput/CustomInput.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './CustomInput.css'
 
 function CustomInput(props) {
-    console.log(props);
     return (
         <div className="input-box">
             <input
