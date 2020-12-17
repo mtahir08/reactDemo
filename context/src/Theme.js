2b0f4b500facc2cecfa7a5ab737649d9cb93e7dc
Muhammad Tahir <mtahir.ps@gmail.com>
2020-12-17 19:42:54 +0500

    theme

---
+++ context/src/Theme.js
@@ -0,0 +1,13 @@
+
+const AppTheme = {
+  light: {
+      textColor: "#000",
+      backgroundColor: "#fff"
+  },
+  dark: {
+      textColor: "#fff",
+      backgroundColor: "#333"
+  }
+}
+
+export default AppTheme
